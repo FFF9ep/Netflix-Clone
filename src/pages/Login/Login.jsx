78c0c1d91fc43ad36a631bc52c871afdd2ac6f3c
fandi a <fandiiardy@gmail.com>
2024-11-08 13:17:23 +0700

    Update Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
   return (
     <div className='login'>
-      
+      <img src={logo} className='login-logo' alt="" />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your Name' />
+          <input type="password" placeholder='Password' />
+          <input type="email" placeholder='Email' />
+          <button>Sign In</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
