434050d9ab3e482751c2daba9909ab43b5475311
fandi a <fandiiardy@gmail.com>
2024-11-08 10:47:50 +0700

    Update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,6 +23,12 @@ const Home = () => {
           <TitleCards/>
         </div>
       </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"}/>
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming Movies"}/>
+        <TitleCards title={"Top Pics for You"}/>
+      </div>
     </div>
   )
 }
