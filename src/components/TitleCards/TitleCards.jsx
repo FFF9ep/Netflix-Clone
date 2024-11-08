f2203142ba684a4ccf4aa1f87747d1bc12031164
fandi a <fandiiardy@gmail.com>
2024-11-08 17:48:11 +0700

    Add API Movies

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,15 +1,29 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title,category}) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDMyMjY4N2ZiNjc5NmM5MWYwYWJmOTJmMGNkNGZhOCIsIm5iZiI6MTczMTA2MTg2My42MTUwMDcyLCJzdWIiOiI2NzJkZTcxZTRiMzBlOTI5Y2Q2ZjJiMDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0Gnlbzl_fzDsaGsuqkSvB0dKi8IPMXsVSAbVfZlY8Dk'
+    }
+  };
+  
   const handleWheel = (event)=>{
     event.preventDefault;
     cardsRef.current.scrollLeft += event.deltaY;
   }
   useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}now_playing?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
   },[])
 
@@ -17,10 +31,10 @@ const TitleCards = ({title,category}) => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card,index)=>{
+        {apiData.map((card,index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -25,8 +25,7 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          {signState==="Sign In"?<p>New? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now!</span></p>:<p>Already Have An Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now!</span></p>
-          }
+          {signState==="Sign In"?<p>New? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now!</span></p>:<p>Already Have An Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now!</span></p>}
         </div>
       </div>
     </div>
