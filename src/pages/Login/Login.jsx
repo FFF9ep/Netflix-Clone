547574a3962fd6a85ccdb2e93f262e3070908ba7
fandi a <fandiiardy@gmail.com>
2024-11-08 15:43:18 +0700

    Add Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState==="Sing Up"?<input type="text" placeholder='Your Name' />:<></>}          
+          {signState==="Sign Up"?<input type="text" placeholder='Your Name' />:<></>}          
           <input type="password" placeholder='Password' />
           <input type="email" placeholder='Email' />
           <button>{signState}</button>
@@ -25,9 +25,7 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          {signState==="Sign In"?
-          <p>New to Netflix <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now!</span></p>
-          :<p>Already Have An Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now!</span></p>
+          {signState==="Sign In"?<p>New? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now!</span></p>:<p>Already Have An Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now!</span></p>
           }
         </div>
       </div>
