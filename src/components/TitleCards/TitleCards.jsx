f79adb6a7443a8723fd37125a5dd4d331c90e499
fandi a <fandiiardy@gmail.com>
2024-11-09 13:14:50 +0700

    Update Logout and Auth

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -3,7 +3,7 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 import { Link } from 'react-router-dom'
 
-const TitleCards = ({title,category}) => {
+const TitleCards = ({ title, category }) => {
 
   const [apiData, setApiData] = useState([]);
 
@@ -16,26 +16,26 @@ const TitleCards = ({title,category}) => {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDMyMjY4N2ZiNjc5NmM5MWYwYWJmOTJmMGNkNGZhOCIsIm5iZiI6MTczMTA2MTg2My42MTUwMDcyLCJzdWIiOiI2NzJkZTcxZTRiMzBlOTI5Y2Q2ZjJiMDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0Gnlbzl_fzDsaGsuqkSvB0dKi8IPMXsVSAbVfZlY8Dk'
     }
   };
-  
-  const handleWheel = (event)=>{
+
+  const handleWheel = (event) => {
     event.preventDefault;
     cardsRef.current.scrollLeft += event.deltaY;
   }
 
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?now_playing?language=en-US&page=1`, options)
-    .then(res => res.json())
-    .then(res => setApiData(res.results))
-    .catch(err => console.error(err));
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?now_playing?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
 
     cardsRef.current.addEventListener('wheel', handleWheel)
-  },[])
+  }, [])
 
   return (
     <div className='title-cards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {apiData.map((card,index)=>{
+        {apiData.map((card, index) => {
           return <Link to={`/player/${card.id}`} className="card" key={index}>
             <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
             <p>{card.original_title}</p>
